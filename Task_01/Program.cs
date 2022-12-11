f76018a5a7c89e3fe4e95c8dedeae2c6588753f1
Andrey <andrejsatykov@yandex.ru>
2022-12-11 14:05:42 +0300

    Triangle Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая на вход принимает три числа
+// и проверяет существует ли треугольник со сторонами такой длинны.
+
+Console.Clear();
+
+Console.Write("Введите первое число x=  ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число y=  ");
+int y = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число z=  ");
+int z = int.Parse(Console.ReadLine());
+
+if(x + y < z || x + z < y || z + y < x)
+{
+    Console.Write("Соответствует треугольнику!!");
+}
+else
+{
+    Console.Write("Не треугольник!!");
+}
